a676a95548a35fea1f1e4625023be6636dea2cdf
alaanvv <valealan84@gmail.com>
2025-11-24 10:53:33 -0300

    refactor: graph

--- Ants/graph.h
+++ Ants/graph.h
@@ -5,23 +5,24 @@
 #include <stdio.h>
 
 #define ASSERT(x, ...) if (!(x)) { printf(__VA_ARGS__); printf("\n"); exit(1); }
-#define MAX(x, y) (x > y ? x : y)
 
+// Representa um grafo usando matriz de adjacencia
 typedef struct {
-  int** data;
+  int **data;
   int size;
 } Graph;
 
-static inline Graph* graph_new(const char* path, int n) {
-  Graph* g = (Graph*) malloc(sizeof(Graph));
+// Cria um novo grafo a partir de um arquivo de entrada
+static inline Graph *graph_new(const char *path, int n) {
+  Graph *g = (Graph *)malloc(sizeof(Graph));
   g->size = n;
 
-  g->data = (int**) malloc(sizeof(int*) * n);
+  g->data = (int **)malloc(sizeof(int *) * n);
   for (int i = 0; i < n; i++)
-    g->data[i] = (int*) malloc(sizeof(int) * n);
+    g->data[i] = (int *)malloc(sizeof(int) * n);
 
-  FILE* input = fopen(path, "r");
-  ASSERT(input, "Arquivo não encontrado");
+  FILE *input = fopen(path, "r");
+  ASSERT(input, "Arquivo nao encontrado");
 
   for (int i = 0; i < n; i++)
     for (int j = 0; j < n; j++)
@@ -31,32 +32,17 @@ static inline Graph* graph_new(const char* path, int n) {
   return g;
 }
 
-static inline void graph_free(Graph* g) {
+static inline void graph_free(Graph *g) {
   for (int i = 0; i < g->size; i++)
     free(g->data[i]);
   free(g->data);
   free(g);
 }
 
-static inline void graph_add(Graph* g, int a, int b, int w) {
-  g->data[a][b] = w;
-  g->data[b][a] = w;
-}
+// Retorna o tamanho de uma aresta
+static inline int graph_get(Graph *g, int a, int b) { return g->data[a][b]; }
 
-static inline int graph_get(Graph* g, int a, int b) {
-  return g->data[a][b];
-}
-
-static inline int graph_has(Graph* g, int a, int b) {
-  return g->data[a][b] != 0;
-}
-
-static inline void graph_print(Graph* g) {
-  for (int i = 0; i < g->size; i++) {
-    for (int j = 0; j < g->size; j++)
-      printf("%d ", g->data[i][j]);
-    printf("\n");
-  }
-}
+// Verifica se existe uma aresta entre dois nos
+static inline int graph_has(Graph *g, int a, int b) { return g->data[a][b] != 0; }
 
 #endif
