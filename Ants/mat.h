31d823e771e1985719c013ead4f38ffbb4968c1a
alaanvv <valealan84@gmail.com>
2025-11-24 10:44:08 -0300

    refactor: mat

--- Ants/mat.h
+++ Ants/mat.h
@@ -1,53 +1,47 @@
-#ifndef MATRIX_H
-#define MATRIX_H
+#ifndef MAT_H
+#define MAT_H
 
 #include <stdlib.h>
 
 typedef struct {
-  int rows, cols;
   double **m;
+  int size;
 } Matrix;
 
-static inline Matrix* mat_new(int r, int c) {
-  Matrix* x = malloc(sizeof(Matrix));
-  x->rows = r;
-  x->cols = c;
-  x->m = malloc(sizeof(double *) * r);
-  for (int i = 0; i < r; i++)
-    x->m[i] = calloc(c, sizeof(double));
+static inline Matrix *mat_new(int size) {
+  Matrix *x = (Matrix *)malloc(sizeof(Matrix));
+  x->size = size;
+  x->m = (double **)malloc(sizeof(double *) * size);
+  for (int i = 0; i < size; i++)
+    x->m[i] = (double *)calloc(size, sizeof(double));
   return x;
 }
 
-static inline void mat_free(Matrix* x) {
-  for (int i = 0; i < x->rows; i++)
+static inline void mat_free(Matrix *x) {
+  for (int i = 0; i < x->size; i++)
     free(x->m[i]);
   free(x->m);
   free(x);
 }
 
-static inline void mat_set(Matrix* x, int r, int c, double v) {
-  x->m[r][c] = v;
-}
-
-static inline double mat_get(Matrix* x, int r, int c) {
-  return x->m[r][c];
-}
-
-static inline void mat_add(Matrix* a, const Matrix* b) {
-  for (int i = 0; i < a->rows; i++)
-    for (int j = 0; j < a->cols; j++)
+// Soma a matriz b na matriz a
+static inline void mat_add(Matrix *a, Matrix *b) {
+  for (int i = 0; i < a->size; i++)
+    for (int j = 0; j < a->size; j++)
       a->m[i][j] += b->m[i][j];
 }
 
-static inline void mat_scale(Matrix* a, double k) {
-  for (int i = 0; i < a->rows; i++)
-    for (int j = 0; j < a->cols; j++)
+// Multiplica uma matriz por um escalar k
+static inline void mat_scale(Matrix *a, double k) {
+  for (int i = 0; i < a->size; i++)
+    for (int j = 0; j < a->size; j++)
       a->m[i][j] *= k;
 }
 
-static inline void mat_fill(Matrix* a, double v) {
-  for (int i = 0; i < a->rows; i++)
-    for (int j = 0; j < a->cols; j++)
+// Preenche uma matriz com um valor v
+static inline void mat_fill(Matrix *a, double v) {
+  for (int i = 0; i < a->size; i++)
+    for (int j = 0; j < a->size; j++)
       a->m[i][j] = v;
 }
 
