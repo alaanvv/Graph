ccd4fb0b8a5b2009680a26e84975bd61741de00d
alaanvv <valealan84@gmail.com>
2025-11-24 10:33:52 -0300

    refactor: tour

--- Ants/tour.h
+++ Ants/tour.h
@@ -1,59 +1,58 @@
 #ifndef TOUR_H
 #define TOUR_H
 
-#include <stdlib.h>
 #include "graph.h"
 
+// Representa um caminho
 typedef struct {
-  int* nodes;
-  int count;
-  int capacity;
-  int length;
+  int *nodes; // Lista de nos visitados
+  int count;  // Numero de nos visitados
+  int length; // Comprimento total do caminho
 } Tour;
 
-static inline Tour* tour_new(int capacity) {
-  Tour* t = (Tour*) malloc(sizeof(Tour));
-  t->nodes = (int*) malloc(sizeof(int) * capacity);
-  t->count = 0;
-  t->capacity = capacity;
+static inline Tour *tour_new(int size) {
+  Tour *t = (Tour *)malloc(sizeof(Tour));
+  t->nodes = (int *)malloc(sizeof(int) * size);
   t->length = 0;
+  t->count = 0;
   return t;
 }
 
-static inline void tour_free(Tour* t) {
+static inline void tour_free(Tour *t) {
   free(t->nodes);
   free(t);
 }
 
-static inline void tour_reset(Tour* t) {
-  t->count = 0;
-  t->length = 0;
-}
-
-static inline void tour_add(Tour* t, int node, const Graph* g) {
-  if (t->count > 0) {
-    int prev = t->nodes[t->count - 1];
-    t->length += graph_get((Graph*) g, prev, node);
-  }
+// Adiciona um no ao tour e atualiza o comprimento total baseado na aresta do grafo g
+static inline void tour_add(Tour *t, int node, Graph *g) {
   t->nodes[t->count++] = node;
+  if (t->count > 1) t->length += graph_get((Graph *)g, t->nodes[t->count - 2], node);
 }
 
-static inline int tour_close(Tour* t, const Graph* g) {
+// Fecha o tour, adicionando o tamanho da aresta de volta ao no inicial
+static inline int tour_end(Tour *t, Graph *g) {
   if (t->count <= 1) return 0;
-  int first = t->nodes[0];
-  int last  = t->nodes[t->count - 1];
-  t->length += graph_get((Graph*) g, last, first);
+  t->length += graph_get((Graph *)g, t->nodes[t->count - 1], t->nodes[0]);
   return t->length;
 }
 
-static inline void tour_cpy(Tour* dst, const Tour* src) {
+// Copia os dados de um tour para outro
+static inline void tour_cpy(Tour *dst, Tour *src) {
   dst->count = src->count;
   dst->length = src->length;
   for (int i = 0; i < src->count; i++)
     dst->nodes[i] = src->nodes[i];
 }
 
-static inline int tour_has(const Tour* t, int node) {
+// Duplica os dados de um tour
+static inline Tour *tour_dupe(Tour *src) {
+  Tour* dupe = tour_new(src->count);
+  tour_cpy(dupe, src);
+  return dupe;
+}
+
+// Verifica se um no ja foi visitado
+static inline int tour_has(Tour *t, int node) {
   for (int i = 0; i < t->count; i++)
     if (t->nodes[i] == node)
       return 1;
